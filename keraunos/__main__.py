f1af6b45c65ef9cfe628d4ef76a3b8ad275c11e1
Volatus <volatusCoder@outlook.com>
2022-04-21 21:12:39 -0300

    Read initial extensions from config file

--- keraunos/__main__.py
+++ keraunos/__main__.py
@@ -1,4 +1,5 @@
 from os import getenv
+from configparser import ConfigParser
 
 from discord.ext import commands
 
@@ -11,12 +12,15 @@ setup_logging()
 
 class Keraunos(commands.Bot):
     def run(self):
-        # (temporary) manually load extensions
-        bot.load_extension("keraunos.extensions.engineer")
-        bot.load_extension("keraunos.extensions.fallacies")
-        bot.load_extension("keraunos.extensions.funny")
-
+        self.config_init()
         super().run(getenv("TOKEN"))
+
+    def config_init(self):
+        self.config = ConfigParser()
+        self.config.read("keraunos.conf")
+
+        for ext in self.config["keraunos.init"]["extensions"].split(","):
+            self.load_extension(f"keraunos.extensions.{ext}")
 
     async def on_connect(self):
         logger.info("Bot connected")
